731ef80413b5aa46f494d50f19cc9460510643d5
Jamie Gaehring <gaehj457@gmail.com>
2019-03-15 16:22:41 -0400

    convert App to functional component

--- examples/farmos-react/src/App.js
+++ examples/farmos-react/src/App.js
@@ -1,36 +1,34 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import farmOS from 'farmos';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  componentDidMount() {
+const App = () => {
+  useEffect(() => {
     const host = '';
     const username = 'farmos';
     const password = 'farmos';
     const farm = farmOS(host, username, password);
     farm.authenticate().then(console.log);
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+  })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
           >
             Learn React
           </a>
         </header>
       </div>
     );
-  }
 }
 
 export default App;
